39a23794628bcb7e7d21d8a46f5b47aaac9b3453
Notos <lianggh159@outlook.com>
2026-03-17 22:29:29 +0800

    chore: split props type

--- app/components/md-wrapper.tsx
+++ app/components/md-wrapper.tsx
@@ -1,24 +1,26 @@
 import type { ReactNode } from 'react'
 import { formatDate } from '~/logics'
 
-interface MarkdownWrapperProps {
+interface MdxFrontmatter {
+  title: string
+  date?: string
+  description?: string
+  [key: string]: any
+}
+
+interface MdxWrapperProps {
   children: ReactNode
-  frontmatter: {
-    title: string
-    date?: string
-    description?: string
-    [key: string]: any
-  }
+  frontmatter: MdxFrontmatter
 }
 
 // set meta for the wrapper itself, it will be merged with the meta from mdx file
-export function meta({ frontmatter }: { frontmatter: MarkdownWrapperProps['frontmatter'] }) {
+export function meta({ frontmatter }: { frontmatter: MdxFrontmatter }) {
   return [
     { title: frontmatter.title },
   ]
 }
 
-export default function MarkdownWrapper({ children, frontmatter }: MarkdownWrapperProps) {
+export default function MarkdownWrapper({ children, frontmatter }: MdxWrapperProps) {
   return (
     <div className="page-post prose dark:prose-invert">
       {frontmatter.title && (
@@ -35,6 +37,7 @@ export default function MarkdownWrapper({ children, frontmatter }: MarkdownWrapp
           )
         : null}
       <article className="post">
+        {/* MDX content will be rendered here */}
         {children}
       </article>
     </div>
